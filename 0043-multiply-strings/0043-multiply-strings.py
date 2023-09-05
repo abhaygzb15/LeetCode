32a5388c6b59414b809dbf7a317fd781b8b57bc7
abhaygzb15 <121966946+abhaygzb15@users.noreply.github.com>
2023-09-05 19:55:30 +0530

    Time: 13 ms (85.81%), Space: 13.6 MB (8.62%) - LeetHub

---
+++ 0043-multiply-strings/0043-multiply-strings.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def multiply(self, num1, num2):
+        """
+        :type num1: str
+        :type num2: str
+        :rtype: str
+        """
+        num11=int(num1)
+        num22=int(num2)
+        product=num11*num22
+        newstr=str(product)
+        return newstr
+        
