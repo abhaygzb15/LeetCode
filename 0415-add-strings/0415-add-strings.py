64f02eb4bde8fe947ef2dceccdf5c396f17df3a6
abhaygzb15 <121966946+abhaygzb15@users.noreply.github.com>
2023-09-10 15:33:26 +0530

    Time: 23 ms (46.92%), Space: 13.6 MB (52.89%) - LeetHub

---
+++ 0415-add-strings/0415-add-strings.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def addStrings(self, num1, num2):
+        """
+        :type num1: str
+        :type num2: str
+        :rtype: str
+        """
+        st1=int(num1)
+        st2=int(num2)
+        res=st1+st2
+        return str(res)
